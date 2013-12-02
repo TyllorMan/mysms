0078db512798dbf3dc2952629818e0aae1262a62
Tobias <tiagotobias2003@gmail.com>
2013-12-02 14:41:23 -0300

    adicionando pag para contact

---
+++ pages/contact_list.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My SMS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/css/parallax-slider.css" rel="stylesheet">
+    <link href="../assets/css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+    	<div class="container">
+
+    		<!-- Brand and toggle get grouped for better mobile display -->
+			  <div class="navbar-header">
+			    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="brand">
+			      <span class="sr-only">Toggle navigation</span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			    </button>
+			    <a class="navbar-brand" href="#">MySMS </a>
+			  </div>
+
+			  <div class="collapse navbar-collapse pull-right" id="nav-login">
+			    <ul class="nav navbar-nav">
+			      <li class="active" ><a href="#">Bem vindo User 1</a></li>
+			    </ul>
+			  </div><!-- /.navbar-collapse -->
+
+
+			</div>
+
+		</nav>
+
+    <div class="container">
+      <div class="page-header">
+        <a href="create_contact.php" class="btn btn-success pull-right"><span class="glyphicon glyphicon-plus-sign"></span>  Create</a>
+        <h1>My messages <small> Lorem Ipsum</small></h1>
+      </div>
+
+      <div class="row">
+        <div class="col-md-2">
+          <ul class="nav nav-pills nav-stacked">
+              <li><a href="message_list.php">Messages</a></li>
+              <li class="active"><a href="#">Contact</a></li>
+          </ul>
+
+
+        </div>
+        <div class="col-md-10">
+          <div class="page-header">
+            <h3>List contacts <small> Lorem Ipsum</small></h3>
+          </div>
+        <table class="table table-hover">
+        <thead>
+          <tr>
+            <th><input type="checkbox"></th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phones</th>
+            <th>
+              <button type="button" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-pencil"></span> Edit</button>
+              <button type="button" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span> Delete</button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><input type="checkbox"></td>
+            <td>Contact 1</td>
+            <td>johnDoe@mail.com</td>
+            <td>9999-0000 </td>
+            <td> </td>
+          </tr>
+          <tr>
+            <td><input type="checkbox"></td>
+            <td>Contact 2</td>
+            <td>johnDoe@mail.com</td>
+            <td>9999-0000 </td>
+            <td> </td>
+          </tr>
+          <tr>
+            <td><input type="checkbox"></td>
+            <td>Contact 3</td>
+            <td>johnDoe@mail.com</td>
+            <td>9999-0000 </td>
+            <td> </td>
+          </tr>
+        </tbody>
+      </table>
+
+        </div>
+      </div>
+      
+
+
+    </div><!-- /.container -->
+      <!-- FOOTER -->
+    <hr class="featurette-divider">
+    <div class="container">
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2013 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/parallax/js/jquery.cslider.js"></script>
+    <script src="../assets/js/parallax/js/modernizr.js"></script>
+    <script src="../assets/js/index.js"></script>
+  
+  </body>
+</html>
---
+++ pages/create_contact.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My SMS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/css/parallax-slider.css" rel="stylesheet">
+    <link href="../assets/css/custom.css" rel="stylesheet">
+    <link href="../assets/css/bootstrap-select.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+    	<div class="container">
+
+    		<!-- Brand and toggle get grouped for better mobile display -->
+			  <div class="navbar-header">
+			    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="brand">
+			      <span class="sr-only">Toggle navigation</span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			    </button>
+			    <a class="navbar-brand" href="#">MySMS </a>
+			  </div>
+
+			  <div class="collapse navbar-collapse pull-right" id="nav-login">
+			    <ul class="nav navbar-nav">
+			      <li class="active" ><a href="#">Bem vindo User 1</a></li>
+			    </ul>
+			  </div><!-- /.navbar-collapse -->
+
+
+			</div>
+
+		</nav>
+
+    <div class="container">
+      <div class="page-header">
+        <h1>New Contatc <small> Lorem Ipsum</small></h1>
+      </div>
+
+      <div class="row">
+        <div class="col-md-2">
+          <ul class="nav nav-pills nav-stacked">
+              <li><a href="message_list.php">Messages</a></li>
+              <li class="active"><a href="contact_list.php">Contact</a></li>
+          </ul>
+        </div>
+
+        <div class="col-md-10">
+          <div class="page-header">
+            <h3>New Contact <small> Lorem Ipsum</small></h3>
+          </div>
+       <form role="form">
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" placeholder="Input Name">
+          </div>
+
+          <div class="form-group">
+            <label for="email">email</label>
+            <input type="email" class="form-control" id="email" placeholder="Input email">
+          </div>
+          
+          <div class="form-group">
+            <label for="bs3Select">Contacts selected</label>
+              <select id="bs3Select" class="selectpicker show-tick form-control" multiple data-live-search="true">
+                  <optgroup label="home" data-subtext="home" data-icon="icon-ok">
+                    <option>cow</option>
+                    <option>bull</option>
+                    <option class="get-class" disabled>ox</option>
+                  </optgroup>
+                  <optgroup label="work" data-subtext="work" data-icon="icon-ok">
+                      <option>ASD</option>
+                      <option>Bla</option>
+                      <option>Ble</option>
+                  </optgroup>
+              </select>
+            </div>
+          <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-plus-sign"></span>  Submit</button>
+        </form>
+      </div>
+      
+
+
+    </div><!-- /.container -->
+      <!-- FOOTER -->
+    <hr class="featurette-divider">
+    <div class="container">
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2013 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/parallax/js/jquery.cslider.js"></script>
+    <script src="../assets/js/parallax/js/modernizr.js"></script>
+    <script src="../assets/js/index.js"></script>
+    <script src="../assets/js/bootstrap-select.min.js"></script>
+
+    <script type="text/javascript">
+	    jQuery(document).ready(function() {
+            $('.selectpicker').selectpicker();    
+	    });
+		</script>
+  
+  </body>
+</html>
